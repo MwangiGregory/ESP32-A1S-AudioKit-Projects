a1850005363aa859192d9bfa61d1f8defd5a5f46
MwangiGregory <gmwaburi@gmail.com>
2024-04-12 23:34:20 +0300

    Add documentation to code

--- player_sd_audiokit/player_sd_audiokit.ino
+++ player_sd_audiokit/player_sd_audiokit.ino
@@ -1,28 +1,32 @@
-// Play MP3 audio files from the SD card. Output is sent to any speaker connected to line out port
+// Play MP3 audio files from the SD card. Output is sent to any speaker channels and to line out port
 
-#include "AudioTools.h"                 // Main library
-#include "AudioLibs/AudioBoardStream.h" // Provides access to boards line in/out
-#include "AudioLibs/AudioSourceSD.h"    // SD card library
-#include "AudioCodecs/CodecMP3Helix.h"  // MP3 decoding/encoding library
+/**
+ * Full list of API
+ *
+ * class AudioBoardStream - https://pschatzmann.github.io/arduino-audio-tools/classaudio__tools_1_1_audio_board_stream.html
+ * class AudioPlayer - https://github.com/pschatzmann/arduino-audio-tools/wiki/The-Audio-Player-Class
+ * class AudioSourceSD - https://pschatzmann.github.io/arduino-audio-tools/classaudio__tools_1_1_audio_source_s_d.html
+ */
 
-const char *startFilePath = "/"; // Start path reading audio files
-const char *ext = "mp3";         // Audio files extension
+#include "AudioTools.h" // https://github.com/pschatzmann/arduino-audio-tools
+#include "AudioLibs/AudioBoardStream.h"
+#include "AudioLibs/AudioSourceSD.h"
+#include "AudioCodecs/CodecMP3Helix.h"
 
-AudioSourceSD source(startFilePath, ext, PIN_AUDIO_KIT_SD_CARD_CS); // SD card
-AudioBoardStream kit(AudioKitEs8388V1);                             // Audio source stream
+const char *startFilePath = "/";
+const char *ext = "mp3"; // Audio files extension
+
+AudioSourceSD source(startFilePath, ext, PIN_AUDIO_KIT_SD_CARD_CS); // AudioSource for AudioPlayer using an SD card as data source
+AudioBoardStream kit(AudioKitEs8388V1);                             // Configures DAC to output to line out and speaker channels
 MP3DecoderHelix decoder;                                            // Object used to decode mp3 to PCM
-
-// Get audio data from sd, decode mp3 to PCM, then pass audio to line out(earphone, speaker channels)
-AudioPlayer player(source, kit, decoder);
+AudioPlayer player(source, kit, decoder);                           // Controls play back
 
 // Audio PlayBack call back functions
-// Play next audio file
 void next(bool, int, void *)
 {
    player.next();
 }
 
-// Play previous audio file
 void previous(bool, int, void *)
 {
    player.previous();
@@ -39,16 +43,18 @@ void setup()
    Serial.begin(115200);
 
    // Start logger at Level INFO
+   // If you need to see more details you can switch to the log level Debug and after things work,
+   // you need to switch to Warning or Error to avoid any negative impact on the audio quality.
    AudioLogger::instance().begin(Serial, AudioLogger::Info);
 
    // setup output
-   // Get default config and set kit to be source of Audio Data(TX_MODE)
+   // Microcontroller is the Audio Source in TX_MODE or Audio Sink in RX_MODE. RXTX_MODE is Source and Sink at the same time!
    auto cfg = kit.defaultConfig(TX_MODE);
    cfg.sd_active = true; // Activate SD card
-   kit.begin(cfg);       // Start line out
+   kit.begin(cfg);       // Start line out with edited configuration
 
    // setup additional buttons
-   kit.addDefaultActions();
+   kit.addDefaultActions(); // Setup the supported default actions (volume, headphone detection)
    kit.addAction(kit.getKey(1), startStop);
    kit.addAction(kit.getKey(4), next);
    kit.addAction(kit.getKey(3), previous);
@@ -60,8 +66,8 @@ void setup()
 
 void loop()
 {
-   // Run very fast functions/logic and avoid delays to prevent slowing down data transfer to audio output device
+   // NOTE: Run very fast functions/logic and avoid delays to prevent slowing down data transfer to audio output device
 
    player.copy();        // Continuously copy data from sd to line out
-   kit.processActions(); // Check for pressed keys
+   kit.processActions(); // Check for pressed keys. This is a bit sluggish, hold keys till action has been acted on.
 }
